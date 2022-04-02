b9eebd8d9eeb530fc9450b92e22ef2239249b5d5
Ricardo Delfin <me@rdelfin.com>
2022-04-02 13:07:10 +0100

    Added stub add sync method

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpServer, Responder, Result};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct SyncRequest {
+    oncall_name: String,
+    user_group: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct SyncResponse {
+    ok: bool,
+}
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
 }
 
-#[actix_web::main] // or #[tokio::main]
+#[post("/add_sync")]
+async fn add_sync(info: web::Json<SyncRequest>) -> Result<impl Responder> {
+    Ok(web::Json(SyncResponse { ok: true }))
+}
+
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(greet))
+    HttpServer::new(|| App::new().service(greet).service(add_sync))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
