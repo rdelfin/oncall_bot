28fa0fd18b65746e1a08aa23b8b59bc33d69f270
Ricardo Delfin <me@rdelfin.com>
2022-04-18 13:31:56 -0500

    Separated out pages into components

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import AppHeader from "./Header";
+import Home from "./Home";
+import Users from "./Users";
+import Oncalls from "./Oncalls";
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
@@ -10,7 +13,6 @@ export default function App() {
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/users" element={<h1>Users</h1>} />
         <Route path="/oncalls" element={<h1>Oncalls</h1>} />
-        <Route path="about" element={<h1>About</h1>} />
       </Routes>
     </div>
   );
---
+++ web/src/Oncalls.tsx
@@ -0,0 +1,3 @@
+export default function Oncalls() {
+  return <h1>Oncalls</h1>;
+}
