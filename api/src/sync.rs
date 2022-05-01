55103d0e1e016895bf0b2123cabaed3166304abe
Ricardo Delfin <me@rdelfin.com>
2022-05-01 22:11:01 +0100

    Added better destruction of syncer

--- api/src/sync.rs
+++ api/src/sync.rs
@@ -2,31 +2,64 @@ use crate::{db, opsgenie, slack};
 use log::{info, warn};
 use std::time::Duration;
 use tokio::{
-    task::{JoinError, JoinHandle},
+    sync::oneshot::{self, error::TryRecvError, Receiver, Sender},
     time::sleep,
 };
 
 #[derive(Debug)]
 pub struct Syncer {
-    jh: JoinHandle<()>,
+    oncall_id: String,
+    user_group_id: String,
+    stop_tx: Option<Sender<()>>,
 }
 
 impl Syncer {
     pub fn new(oncall_id: String, user_group_id: String) -> Syncer {
+        let (stop_tx, stop_rx) = oneshot::channel();
+        let oncall_id_clone = oncall_id.clone();
+        let user_group_id_clone = user_group_id.clone();
+        tokio::spawn(
+            async move { oncall_sync(oncall_id_clone, user_group_id_clone, stop_rx).await },
+        );
         Syncer {
-            jh: tokio::spawn(async move { oncall_sync(oncall_id, user_group_id).await }),
+            stop_tx: Some(stop_tx),
+            user_group_id,
+            oncall_id,
         }
-    }
-
-    #[allow(dead_code)]
-    pub async fn wait(self) -> Result<(), JoinError> {
-        self.jh.await
     }
 }
 
-async fn oncall_sync(oncall_id: String, user_group_id: String) {
+impl Drop for Syncer {
+    fn drop(&mut self) {
+        // Extract out `stop_tx` (which should never be none)
+        match std::mem::take(&mut self.stop_tx) {
+            Some(stop_tx) => {
+                // Sending a stop will be sufficient to stop the oncall syncer on the next iteration
+                if let Err(_) = stop_tx.send(()) {
+                    warn!("Syncer for oncall ID \"{}\" and user group ID \"{}\" failed to send stop. It's likely something went wrong with the syncer task.", self.oncall_id, self.user_group_id);
+                }
+            }
+            None => {
+                warn!("self.stop_tx for syncer with oncall ID \"{}\" and user group ID \"{}\" was none. Did you double-drop?", self.oncall_id, self.user_group_id);
+            }
+        }
+    }
+}
+
+async fn oncall_sync(oncall_id: String, user_group_id: String, mut stop_rx: Receiver<()>) {
     let sleep_time = Duration::from_secs(60);
+
     loop {
+        // First, make sure we haven't gotten a stop message
+        match stop_rx.try_recv() {
+            // We've been asked to stop, or the process above is dead. Stop, so return immediately
+            Ok(_) | Err(TryRecvError::Closed) => {
+                return;
+            }
+            // Do nothing if empty
+            Err(TryRecvError::Empty) => {}
+        };
+
         info!(
             "Updating oncall_id {} and user_group_id {}",
             oncall_id, user_group_id
